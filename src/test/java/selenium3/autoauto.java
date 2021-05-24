22a1a496c99f2e036f7c9a6cc348588a581e6e70
Mohamed.yehia <mohamed.yehia>
2021-05-25 00:48:04 +0200

    kjhkjhkjh

--- src/test/java/selenium3/autoauto.java
+++ src/test/java/selenium3/autoauto.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class autoauto {
+public class autoautcxbvnmcxbvmcxo {
     public static void main(String[] args) {
 
         String[][] data = {
