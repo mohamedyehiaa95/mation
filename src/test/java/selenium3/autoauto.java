73650fe676397f1528d417cb3b30353c2fac50e2
Mohamed.yehia <mohamed.yehia>
2021-06-07 12:34:53 +0200

    new update merge without

--- src/test/java/selenium3/autoauto.java
+++ src/test/java/selenium3/autoauto.java
@@ -5,10 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class autoauto {
+public class autoauto123 {
     public static void main(String[] args) {
 
-        String[][] data = {
+        String[][] 123data = {
 
                 // first test case (  existing email address.)
                 {"Mohamed", "Yehia","01122620999","myeiha7@gmail.com","Y01234567","Y01234567",""},
